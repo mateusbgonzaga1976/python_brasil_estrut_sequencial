9380bc179ce7a35876301f2fa9abef6dc9bb95d1
mateusbgonzaga1976 <mateusbgonzaga@gmail.com>
2023-02-09 11:01:14 -0300

    Exercicio 8 - estrutura sequencial pythonbrasil

---
+++ exerc8.py
@@ -0,0 +1,6 @@
+print('Digite o numero de horas trabalhadas por mes: ')
+horas_trab = float(input())
+print('Digite o valor da hora trabalhada: ')
+vlr_trab = float(input())
+total_salario = horas_trab * vlr_trab
+print(f'Salario mensal de ${total_salario}')
